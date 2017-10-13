a34e6c6a237818d9041eebef8c082b2742025784
prabu1311 <32513714+prabu1311@users.noreply.github.com>
2017-10-13 18:33:28 +0530

    Add files via upload

---
+++ clas2.py
@@ -0,0 +1,17 @@
+class User:
+    name = ""
+ 
+    def __init__(self, name):
+        self.name = name
+ 
+    def sayHello(self):
+        print "Hello, my name is " + self.name
+ 
+# create virtual objects
+james = User("James")
+david = User("David")
+eric = User("Eric")
+ 
+# call methods owned by virtual objects
+james.sayHello()
+david.sayHello()
---
+++ if else.py
@@ -0,0 +1,8 @@
+age= 24
+print "guess my age,you have one chance"
+guess = int(raw_input("guess:"))
+
+if guess != age:
+    print "wrong"
+else:
+    print "correct"
