15d313c9faae62c0e964b0393ba49fa7f114a5a7
prabumukundan <prabumukundan@gmail.com>
2017-10-24 18:40:12 +0530

    exercise

---
+++ age ex.py
@@ -0,0 +1,4 @@
+age = raw_input("whats your age")
+height = raw_input("your height is")
+weight = raw_input("weght is")
+print "so u are %r old, and %r inches, u are %r pounds" % (age,height,weight)
