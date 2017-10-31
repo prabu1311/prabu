029b82754bc24c622558dfc5106d315292a041f8
prabumukundan <prabumukundan@gmail.com>
2017-10-31 18:18:33 +0530

    new

---
+++ newexe/fish1.py
@@ -0,0 +1,29 @@
+
+class fish:
+    list1 =[]
+    list2 =[]
+    def types_of_fish(self):
+        name = str(raw_input("name of fish:"))
+        list1.append(name)
+        tes.write(list1)
+def size_of_fish():
+    size = int(raw_input("size of fish is"))
+    list2.append(size)
+    thefile.write(str(j))
+    #return size
+
+thefile = open('tes.py','w')   
+bass = fish()
+bass.types_of_fish()
+size_of_fish(thefile)
+##
+##
+sunfish = fish()
+sunfish.types_of_fish()
+size_of_fish()
+##
+##
+pike = fish()
+pike.types_of_fish()
+size_of_fish()
+
