03d7d7704d4e71453e6e7fa7ffdb939452d936d6
prabumukundan <prabumukundan@gmail.com>
2017-11-06 19:20:23 +0530

    problem1

---
+++ problem1.py
@@ -0,0 +1,16 @@
+from HTMLParser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag,attr):
+        print " ", tag
+
+        for i in attr:
+            for j in i:
+                print j
+          
+# instantiate the parser and fed it some HTML
+feed= open("html.txt","r")
+data=feed.read()
+parser = MyHTMLParser()
+parser.feed(data)
