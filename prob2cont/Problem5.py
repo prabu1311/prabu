58982849a687c0adc02ea532f8c4b8e2b7a2772a
prabumukundan <prabumukundan@gmail.com>
2017-11-14 19:35:24 +0530

    programs

--- prob2cont/Problem5.py
+++ prob2cont/Problem5.py
@@ -1,78 +1,88 @@
 import re
-from sys import argv
+import sys
 
-script,file_name = argv
-
-class prob:
-
+class prob():
     def check(self,fname):
-	self.fname = file_name
-        if '.html' in file_name or'.txt' in file_name:
-            print "you have entered the correct file"
+        ischoice = True
+        
+        if '.html' in fname or'.txt' in fname:
+            #print "you have entered the correct file"
+            ischoice = True
         else:
             print "you have entered the wrong file"
-        return fname
+            ischoice == False 
+        fopen=open(fname, "r")
+        rf=fopen.read()    
+        return rf,ischoice
 
-    def operation(self):
-        self.TagList=[]
-        self.AttrList=[]
-        self.ValList=[]
-        
+    def operation(self,rf):
+        ischoice = True
+        try:
+            self.TagList=[]
+            self.AttrList=[]
+            self.ValList=[]
+            lis=[]
+    
+            text = re.sub(r'<!.+-->', r' ', rf)
 
-        f=open(file_name ,'r')
-        data = f.read()
-        text = re.sub(r'<!.+-->',r' ',data)
+            for i in re.findall(r'<([^/][^>]*)>', text):
+                if ' ' in i:
+                    for ht in re.findall('([a-z]+)? *([a-z-]+)="([^"]+)', i):
+                        self.TagList.append(ht[0])
+                        self.AttrList.append(ht[1])
+                        self.ValList.append(ht[2])
+                else:
+                    self.TagList.append(i)
 
-        for i in re.findall(r'<([^/][^>]*)>', text):
-            if ' ' in i:
-                for ht in re.findall('([a-z]+)? *([a-z-]+)="([^"]+)',i):
-                    self.TagList.append(ht[0])
-                    self.AttrList.append(ht[1])
-                    self.ValList.append(ht[2])
-                       
-                     
-            else:
-                self.TagList.append(i)
+            lis=self.TagList+self.AttrList+self.ValList
+            self.TagList = filter(None,self.TagList)
 
-        
-        self.TagList = filter(None,self.TagList)        
-        print self.TagList
-        print self.AttrList
-        print self.ValList
+        except:
+            ischoice == False      
+        return lis,ischoice
 
     def display(self):
-	ischoice = True
-        choice=int(raw_input("Enter your choice 1,2,3,4"))
-    
-        if choice==1:
-            for tag in self.TagList:
-                print tag
-            self.display()   
+        ischoice = True
+        try:
+            choice=int(raw_input("Enter your choice 1,2,3,4"))
+        
+            if choice==1:
+                for tag in self.TagList:
+                    print tag
+                self.display()   
 
-        elif choice==2:
-            for att in self.AttrList:
-                print att
-            self.display()   
-        elif choice==3:
-            if len(self.AttrList)==len(self.ValList):
-                for ind in range(0,len(self.AttrList)):
-                    print self.AttrList[ind],"=",self.ValList[ind]
-            self.display()
-        elif choice==4:
-            ans=raw_input("Enter any key:")
-            if ans in self.AttrList:
-                val=self.AttrList.index(ans)
-                print self.AttrList[val],"=",self.ValList[val]
-            else:
-                print "Enter the correct key"
-		return ischoice
+            elif choice==2:
+                for att in self.AttrList:
+                    print att
+                self.display()   
+            elif choice==3:
+                if len(self.AttrList)==len(self.ValList):
+                    for ind in range(0,len(self.AttrList)):
+                        print self.AttrList[ind],"=",self.ValList[ind]
                 self.display()
-        
-        
-        
+            elif choice==4:
+                ans=raw_input("Enter any key:")
+                if ans in self.AttrList:
+                    val=self.AttrList.index(ans)
+                    print self.AttrList[val],"=",self.ValList[val]
+                else:
+                    print "Enter the correct key"
+                    self.display()
+
+        except:
+            ischoice == False 
+
+        return ischoice
+                   
+if __name__== '__main__':
+    if len(sys.argv)>1:
+        h = prob()
+        arg,arg2=h.check(sys.argv[1])
+        if arg2:
+            val1,val2=h.operation(arg)
+        if val2:
+            h.display()
 
 
-h = prob()
-h.check(file_name)
-h.operation()
-h.display()
+
+    
---
+++ prob2cont/testcace.py
@@ -0,0 +1,27 @@
+import unittest
+from Problem5 import prob
+class Testprob (unittest.TestCase):
+    
+    def test_check(self):
+        
+        value = p.check("html.txt")[1]
+        self.assertTrue(value,True)
+  
+    def test_operation(self):
+        result = p.operation("html.txt")[1]
+        self.assertTrue(result,True)
+  
+  
+        #self.assertEqual(result, )
+        
+    #def test_display(self):
+
+        #dis = p.display()
+        #check = True
+        #self.assertEqual(p.display(),check)
+    
+
+if __name__ == '__main__':
+    p =prob()
+    unittest.main()
+    
